d6a403d96284d9f30cf01136ad0b744d42b7731c
Gerrit Louis <gerritlouis@gmail.com>
2018-03-24 13:21:38 +0100

    structure to mvc

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Gerrit Louis on 24-03-18.
+//  Copyright © 2018 Gerrit Louis de Beuze. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var done: Bool = false
+}
